03655ff1f5db5bee34d6d2af58f3654ff6acfc51
ImAdamTM <adam@adamhartwig.co.uk>
2026-03-08 12:24:59 -0400

    feat: 🎸 adjust labels when read to be brighter

--- Preread/Views/Components/ArticleRowView.swift
+++ Preread/Views/Components/ArticleRowView.swift
@@ -27,7 +27,7 @@ struct ArticleRowView: View {
                 VStack(alignment: .leading, spacing: 4) {
                     Text(article.title)
                         .font(Theme.scaledFont(size: 17, weight: article.isRead ? .medium : .semibold))
-                        .foregroundColor(article.isRead ? Theme.textSecondary : Theme.textPrimary)
+                        .foregroundColor(article.isRead ? Theme.textPrimary.opacity(0.6) : Theme.textPrimary)
                         .lineLimit(2)
                         .matchedGeometryEffect(id: article.id.uuidString + "-title", in: namespace)
 
