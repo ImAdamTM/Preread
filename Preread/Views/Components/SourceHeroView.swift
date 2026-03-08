a28ec0ed67373e0ca290ca2cb5b7b1d3e9d1ea96
ImAdamTM <adam@adamhartwig.co.uk>
2026-03-08 10:42:10 -0400

    fix: 🐛 bugfixes

--- Preread/Views/Components/SourceHeroView.swift
+++ Preread/Views/Components/SourceHeroView.swift
@@ -58,10 +58,16 @@ struct SourceHeroView: View {
         }
         .frame(maxWidth: .infinity)
         .frame(height: 240)
-        .background(alignment: .bottom) {
-            blurredBackground
-                .frame(height: 240)
-                .allowsHitTesting(false)
+        .background {
+            GeometryReader { geo in
+                let scrollY = geo.frame(in: .scrollView(axis: .vertical)).minY
+                let overscroll = max(scrollY, 0)
+                blurredBackground
+                    .frame(height: 240)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .offset(y: -overscroll)
+            }
+            .allowsHitTesting(false)
         }
         .task(id: source.iconURL) {
             guard let iconURL = source.iconURL,
